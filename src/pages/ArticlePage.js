9674642f79207a0cd1fe8f6b577f25469e83e3f8
aknair19 <94102097+aknair19@users.noreply.github.com>
2023-04-04 23:29:51 +0530

    code refactored

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import articles from "./article-content";
